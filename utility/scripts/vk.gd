b187f020886237b4d7375801bafb6c6c449738e6
MaximErevanV3 <maximsmirnov35vologda@gmail.com>
2026-06-04 15:55:55 +0300

    Delete utility/scripts/vk.gd

--- utility/scripts/vk.gd
+++
@@ -1,6 +0,0 @@
-extends Button  # Указываем, что этот скрипт принадлежит кнопке
-
-
-func _on_vk_pressed():
-	var url = "https://vk.com/loxcheviti"  # Замените на нужный вам URL
-	OS.shell_open(url)  # Открывает браузер с указанным URL
